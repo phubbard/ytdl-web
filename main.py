31c696cce941158bf1c05402e6a91cf728b9f40f
Paul Hubbard <paul_hubbard@intuit.com>
2021-04-23 14:48:42 -0700

    IPC broken, this commit is an MVP that forks the DL into a background process; seems to work well.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from __future__ import unicode_literals
 from configparser import ConfigParser
-from dataclasses import dataclass
 import logging
-import multiprocessing
+from multiprocessing import Process
 import os
 from pathlib import Path
 import socket
 import sys
 
-from flask import Flask, make_response, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, url_for
 import youtube_dl
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s %(message)s')
@@ -16,19 +15,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelnam
 app = Flask('ytdl-web-smp')
 app.logger.setLevel(logging.DEBUG)
 
-@dataclass
-class Download:
-    # Class for download info/sync across processes
-    messages: list
-    progress: int = 0
-    size_bytes: int = 0
-    fetched_bytes: int = 0
-
-manager = multiprocessing.managers.SyncManager()
-results = manager.dict()
-
-# Global message buffer - see MyLogger
-buffer = []
 
 # Load per-host config froom config file
 hostname = socket.gethostname()
@@ -45,15 +31,13 @@ except KeyError:
 class MyLogger(object):
     def debug(self, msg):
         pass
-        #buffer.append(msg)
 
     def warning(self, msg):
-        buffer.append(f"WARN {msg}")
-        app.logger.warning(msg)
+        pass
 
     def error(self, msg):
-        buffer.append(f"ERROR {msg}")
-        app.logger.error(msg)
+        pass
+        # app.logger.error(msg)
 
 
 def make_dirlist(parent):
@@ -61,57 +45,38 @@ def make_dirlist(parent):
     all_things = Path(parent)
     all_dirs = [x.name for x in all_things.iterdir() if x.is_dir()]
     # Remove any directories with names starting with a dot
-    filtered_dirs = [x for x in all_dirs if not x.startswith(('.'))]
+    filtered_dirs = [x for x in all_dirs if not x.startswith('.')]
     return sorted(filtered_dirs)
 
 
-def my_hook(d):
-    # Event hook - status seems to be finished or downloading
-    if d['status'] == 'finished':
-        stats = f"{d['_total_bytes_str']} {d['filename']}"
-        app.logger.info(f"Done OK: {stats}")
-        buffer.append(f'Done {stats}')
-    elif d['status'] == 'error':
-        app.logger.error(f"==> {str(d)}")
-        buffer.append(str(d))
+def worker(my_url: str, dest: Path):
+    cur_dir = os.getcwd()
+    try:
+        os.chdir(dest)
+        app.logger.debug(f'Destination looks OK, starting on {my_url}')
+        with youtube_dl.YoutubeDL({'logger': MyLogger()}) as ydl:
+            ydl.download([my_url])
+    except OSError as ose:
+        app.logger.exception(ose)
+    finally:
+        os.chdir(cur_dir)
+    app.logger.info(f"Worker done with {my_url}")
 
-# YTDL options - quality, etc. Defaults work here, so just add hooks.
-ydl_opts = {
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
-}
 
 # Display the to-be-downloaded page
 @app.route('/', methods=['GET'])
 def index():
-    global buffer
-    buffer = []
     return render_template('index.html', dirs=make_dirlist(dest_vol), default_dir=default_dir)
 
 
-# todo display something while downloading
 @app.route('/submit', methods=['POST'])
 def submit():
     url = request.form['vidlink']
     dest_dir = request.form['destination']
-
     dest_path = Path(dest_vol, dest_dir)
-    cur_dir = os.getcwd()
-    try:
-        os.chdir(dest_path)
-        app.logger.debug(f'Destination looks OK, starting on {url}')
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        done_ok = True
-    except OSError as ose:
-        done_ok = False
-        buffer.append(f'Error in target directory {str(ose)}')
-    finally:
-        os.chdir(cur_dir)
-
-    app.logger.info(f'Done {done_ok}')
-    return render_template('results.html', messages=buffer, status=done_ok,
-                           url=url, dest_dir=dest_dir, restart_url=url_for('index'))
+    p = Process(target=worker, args=(url, dest_path))
+    p.start()
+    return render_template('bg.html', restart_url=url_for('index'))
 
 
 if __name__ == '__main__':
--- try-mp.py
+++ try-mp.py
@@ -3,15 +3,12 @@
 # pfh 4/22/2021
 from __future__ import unicode_literals
 from configparser import ConfigParser
-from dataclasses import dataclass
 import logging
-from multiprocessing import Manager, freeze_support, Process
+from multiprocessing import Process
 import os
 from pathlib import Path
 import socket
 import sys
-import typing
-from uuid import uuid4
 
 from flask import Flask, make_response, request, render_template, redirect, url_for
 import youtube_dl
@@ -19,19 +16,6 @@ import youtube_dl
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s %(message)s')
 log = logging.getLogger()
 app = Flask('ytdl-web-smp')
-
-
-@dataclass
-class Download:
-    # Class for download info/sync across processes
-    id: str
-    url: str
-    messages: typing.List[str]
-    progress: int = 0
-    size_bytes: int = 0
-    fetched_bytes: int = 0
-    done: bool = False
-    error: bool = False
 
 
 # Load per-host config from config file
@@ -45,102 +29,68 @@ except KeyError:
     log.error(f"Add a {socket.gethostname()} section in config.ini")
     sys.exit(1)
 
+
 @app.route('/poll/{job_id}')
 def poll(job_id):
-    global results
-    if results[job_id].done == False:
-        return render_template("working.html", progress=results[job_id].progress,
-                               msgs=results[job_id].messages)
+    global jobs
 
-    return render_template('results.html', messages=results[job_id].messages,
-                           status=not results[job_id].error, dest_dir='foo',
-                           restart_url=url_for('index'))
-
-
-class MyLogger(object):
-    def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        pass
-
-def progress_hook(d):
-    # Event hook - see https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L230-L253
-    global result
-    global url
-
-    if d['status'] == 'downloading':
-        result.fetched_bytes = d['downloaded_bytes']
-        result.size_bytes = d['total_bytes']
-        result.progress = 100.0 * (result.fetched_bytes / result.size_bytes)
-        log.info(f"{url} {result.progress}% done")
-    if d['status'] == 'finished':
-        stats = f"{d['_total_bytes_str']} {d['filename']}"
-        log.info(f"Done OK: {stats}")
-        result.messages.append(f'Done {stats}')
-        result.done = True
-    elif d['status'] == 'error':
-        result.messages.append(f'Error {str(d)}')
-        log.error(f"==> {str(d)}")
-        result.done = True
-        result.error = True
+    # Linear search for job id - can't pass URL as path param. FIXME
+    for job in jobs:
+        if job.id == job_id:
+            if not job.done:
+                job.polls += 1
+                if job.polls >= 60:
+                    # TODO
+                    log.error(f"Polling maxed on {job}")
+                else:
+                    return render_template("working.html", progress=job.progress, msgs=job.messages)
+            else:
+                # done! happy path
+                return render_template('results.html', messages=job.messages, status=not job.error,
+                                       dest_dir='FIXME', restart_url=url_for('index'))
+    # Job not found
+    return make_response(f'Unable to locate {job_id}', 403)
 
 
-def worker(my_url: str, proxy_obj: Download, dest: Path):
-    # Per-process globals - the second is a shared proxy object
-    global url
-    global result
-    url = my_url
-    result = proxy_obj
+def worker(my_url: str, dest: Path):
     cur_dir = os.getcwd()
-
     try:
         os.chdir(dest)
-        app.logger.debug(f'Destination looks OK, starting on {url}')
-        with youtube_dl.YoutubeDL({'progress_hooks': [progress_hook],
-                                  'logger': MyLogger()}) as ydl:
-            ydl.download([url])
+        app.logger.debug(f'Destination looks OK, starting on {my_url}')
+        with youtube_dl.YoutubeDL() as ydl:
+            ydl.download([my_url])
     except OSError as ose:
         app.logger.exception(ose)
     finally:
         os.chdir(cur_dir)
 
-    log.info(f"Worker done with {url} {str(result)}")
+    log.info(f"Worker done with {my_url}")
 
 
-def test_explicit(url, dest_dir) -> str:
-
+def test_explicit(url: str, dest_dir: str):
     dest_path = Path(dest_vol, dest_dir)
-    jobid = uuid4().hex
-    d = Download(jobid, url, [])
-    results[jobid] = d
-    p = Process(target=worker, args=(url, d, dest_path))
+    p = Process(target=worker, args=(url, dest_path))
     p.start()
-    return jobid
 
 
-# todo display something while downloading
 @app.route('/submit', methods=['POST'])
 def submit():
     url = request.form['vidlink']
     dest_dir = request.form['destination']
-    jobid = test_explicit(url, dest_dir)
-    return redirect(f'/poll/{jobid}')
+    test_explicit(url, dest_dir)
+    return render_template('bg.html', restart_url=url_for('index'))
 
 
 def tester():
     # Exercise the MP code
-    urls = ['https://www.youtube.com/watch?v=fyyiJc0Wk2M&feature=emb_imp_woyt',
-            'https://youtu.be/yy0zS0ZENkA',
-            'https://youtu.be/QWPytpiml5c']
+    urls = [
+        'https://www.youtube.com/watch?v=fyyiJc0Wk2M&feature=emb_imp_woyt',
+        'https://youtu.be/yy0zS0ZENkA',
+        'https://youtu.be/QWPytpiml5c'
+    ]
     for url in urls:
         test_explicit(url, default_dir)
 
+
 if __name__ == '__main__':
-    manager = Manager()
-    global results
-    results = manager.dict()
     tester()
